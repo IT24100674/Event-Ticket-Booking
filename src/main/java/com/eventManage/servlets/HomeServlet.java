e2fe7ebbc5d83e2b013dd03100cadf37d04e628a
IT24100674 <IT24100674@my.sliit.lk>
2025-05-08 19:42:02 +0530

    up

--- src/main/java/com/eventManage/servlets/HomeServlet.java
+++ src/main/java/com/eventManage/servlets/HomeServlet.java
@@ -1,32 +1,4 @@
-//package com.eventManage.servlets;
-//
-//import com.eventManage.utils.FileParser;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.*;
-//import javax.servlet.RequestDispatcher;
-//import java.io.IOException;
-//import java.util.List;
-//
-//@WebServlet("/home")
-//public class HomeServlet extends HttpServlet {
-//
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//
-//
-//
-//
-//        List<String[]> events = FileParser.readAndParseFile("D:\\SLIIT\\2nd SEM\\OOP\\project\\pro\\src\\main\\database\\evenPost.txt");
-//
-//        request.setAttribute("events", events);
-//
-//        RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
-//        dispatcher.forward(request, response);
-//    }
-//}
+
 package com.eventManage.servlets;
 
 import com.eventManage.model.Event;
--- src/main/java/com/eventManage/utils/FileParser.java
+++ src/main/java/com/eventManage/utils/FileParser.java
@@ -1,33 +1,4 @@
-//package com.eventManage.utils;
-//
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class FileParser {
-//
-//    public static List<String[]> readAndParseFile(String filePath) {
-//        List<String[]> events = new ArrayList<>();
-//
-//        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-//            String line;
-//
-//            while ((line = reader.readLine()) != null) {
-//                String[] parts = line.split(",");
-//                if (parts.length >= 4) {
-//                    events.add(parts);
-//                }
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return events;
-//    }
-//}
+
 package com.eventManage.utils;
 
 import com.eventManage.model.Event;
