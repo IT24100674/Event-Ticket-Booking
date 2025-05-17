6c080540bda89c09f3c93a4e63223e0237528ebc
IT24102793 <IT24102793@my.sliit.lk>
2025-05-17 11:36:03 +0530

    update1

---
+++ src/main/java/com/eventManage/model/Review.java
@@ -0,0 +1,19 @@
+package com.eventManage.model;
+
+public class Review {
+    private String name;
+    private String text;
+
+    public Review(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
