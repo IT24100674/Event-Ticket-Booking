53f59448e4c75f8f8f25e5dc2a6335688a1a5efd
IT24100674 <IT24100674@my.sliit.lk>
2025-05-19 09:01:38 +0530

    up

--- src/main/java/com/eventManage/utils/writeF.java
+++ src/main/java/com/eventManage/utils/writeF.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class writeF {
 
-    // Generic method to write any data to a specified file
+
     public static void writeToFile(String filePath, Event event) {
         try (FileWriter writer = new FileWriter(filePath,true)) {
             writer.write(event.getName()+","+event.getDate()+","+event.getLocation()+","+event.getTicketPrice()+","+event.getImageName()+ "\n");
